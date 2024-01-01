296a72417e2d44142d73bdd2d04cfdf583f32cbc
Osama Bin Azam <osamabinazam@gmail.com>
2024-01-01 11:29:44 +0500

    update timeslot model

--- backend/src/models/timeslot.js
+++ backend/src/models/timeslot.js
@@ -4,14 +4,15 @@ const Schema = mongoose.Schema;
 const timeslotSchema = new Schema({
     startTime: {
         type: String,
-        required: true
     },
     endTime: {
         type: String,
-        required: true
+
     }
 }, {
     timestamps: true
 });
 
+
 const Timeslot = mongoose.model('Timeslot', timeslotSchema);
+module.exports = Timeslot;
