222706dfdab918c8fddea16e5d8eaa1c15b0b8db
Osama Bin Azam <osamabinazam@gmail.com>
2024-01-01 11:27:48 +0500

    Create appointments

--- backend/src/controllers/AppointmentController.js
+++ backend/src/controllers/AppointmentController.js
@@ -20,6 +20,48 @@ exports.appointment_list = async function (req, res) {
 };
 
 // Create an appointment
-exports.appointment_create = function (req, res) {
-    res.send('NOT IMPLEMENTED: appointment create');
+exports.appointment_create =  async function (req, res) {
+    const data = req.body;
+    console.log(data);
+
+    if (!data) {
+        return res.status(400).send({
+            message: "Timeslot can not be empty"
+        });
+    }
+
+    if (!data.fullname) {
+        return res.status(400).send({
+            message: "Timeslot start time can not be empty"
+        });
+    }
+
+    if (!data.timeslot) {
+        return res.status(400).send({
+            message: "Timeslot end time can not be empty"
+        });
+    }
+
+    const appointment = new Appointment(
+        {
+           name: data.fullname,
+           timeslot: data.timeslot,
+           email: data.email
+        }
+   );
+
+   console.log(appointment)
+    try{
+        const createdAppointment = await appointment.save();
+        if (!createdAppointment) {
+            return res.status(500).send({
+                message: "Error creating appointment"
+            });
+        }
+        res.status(200).send(createdAppointment);
+    }catch(err){
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the appointment."
+        });
+    }
 };
